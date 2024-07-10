102510271712f8da2b8b8f8db3340cf923e6fa32
C.Maureder <maureder.christoph@gmail.com>
2024-07-10 20:06:00 +0200

    add shell of scriptable projectile maper
    maper that maps the scriptable object to the instance

--- DefendTower/Assets/_Scripts/Scriptables/ScriptableProjectile.cs
+++ DefendTower/Assets/_Scripts/Scriptables/ScriptableProjectile.cs
@@ -4,6 +4,8 @@
 /// Data storage for projectiles
 /// </summary>
 public class ScriptableProjectile : ScriptableObject {
-
+    public ProjectileInfo GetInfo() {
+        throw new System.NotImplementedException();
+    }
 }
 
